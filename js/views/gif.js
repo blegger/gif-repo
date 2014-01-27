56a25be6b2bfeb3b69e702a7d5f2542b6701dbc2
Craig Boal <craig.boal@gmail.com>
2014-01-26 21:12:05 -0700

    Create the add and delete click handlers

--- js/views/gif.js
+++ js/views/gif.js
@@ -5,6 +5,18 @@ app.GifView = Backbone.View.extend({
     className: 'gif-outerwrapper clearfix',
     template: _.template( $( '#gifTemplate' ).html() ),
 
+	events: {
+        'click .delete': 'deleteGif'
+    },
+
+    deleteGif: function() {
+        //Delete model
+        this.model.destroy();
+
+        //Delete view
+        this.remove();
+    },
+	
     render: function() {
         //this.$el is what we defined in tagName. use $el to get access to jQuery html() function
         this.$el.html( this.template( this.model.toJSON() ) );
--- js/views/library.js
+++ js/views/library.js
@@ -3,11 +3,31 @@ var app = app || {};
 app.LibraryView = Backbone.View.extend({
     el: '#gifs',
 
-    initialize: function( initialGifs ) {
-        this.collection = new app.Library( initialGifs );
+    initialize: function( initialGifs ) {        
+		this.collection = new app.Library( initialGifs );
         this.render();
+		
+		this.listenTo( this.collection, 'add', this.renderGif );
     },
 
+	events:{
+		'click #add':'addGif'
+	},
+
+	addGif: function( e ) {
+		e.preventDefault();
+
+		var formData = {};
+
+		$( '#addGif div' ).children( 'input' ).each( function( i, el ) {
+			if( $( el ).val() != '' ) {
+				formData[ el.id ] = $( el ).val();
+			}
+		});
+
+		this.collection.add( new app.Gif( formData ) );
+	},
+	
     // render library by rendering each GIF in its collection
     render: function() {
         this.collection.each(function( item ) {
